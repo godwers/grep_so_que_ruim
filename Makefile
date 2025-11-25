cca1b87df195d77da313d687542f05689dca8ac7
godwers <gondes.historia@gmail.com>
2025-11-25 17:54:26 -0300

    terminei só o comecinho

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
-test:
+CC = gcc 
+CFLAGS = -O2 -lm
+
+test: main.cpp
 	g++ main.cpp -O2
+	./a.out "penis" testando.txt
+	./a.out "p" testando.txt
+	./a.out "a" testando.txt
+	./a.out "Diabo" igrejadodiabo.txt
--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <fstream>
 #include <iostream>
 
+bool findPattern(std::string text, std::string pattern) {
+  int patternIndex = 0;
+  for (int index = 0; index < text.size(); index++) {
+    if (text[index] == pattern[patternIndex]) {
+      patternIndex++;
+    } else {
+      patternIndex = 0;
+    }
+
+    if (patternIndex == pattern.size() - 1) {
+      return true;
+    }
+  }
+  return false;
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 3) {
-    std::cout << "Not enough arguments\n";
+    std::cout << "Not enough arguments\n" << "[PATTERN] [FILE]";
     return 1;
   }
   const std::string pathFile = argv[2];
@@ -17,10 +33,15 @@ int main(int argc, char *argv[]) {
   }
 
   std::string line;
+  int line_position = 1;
   while (std::getline(file, line)) {
     // TODO
     // tellg() gets the last buffer of the line
     // make the grep algorithm
+    if (findPattern(line, pattern)) {
+      std::cout << line_position << " " << line << "\n";
+    }
+    line_position++;
   }
   file.close();
   return 0;
